fe47d398a09e6aa969cdbf890fda90187f1c0537
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-11 11:26:43 +0300

    upd

--- app/src/main/java/student/testing/system/ui/LaunchActivity.kt
+++ app/src/main/java/student/testing/system/ui/LaunchActivity.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import student.testing.system.R
-import java.io.InputStream
-import java.io.OutputStreamWriter
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launch)
     }
 }
