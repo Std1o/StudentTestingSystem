0c4316bd39322f76514ff4ca451ee4235b35a667
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-12 10:55:50 +0300

    upd

--- app/src/main/java/student/testing/system/ui/LaunchActivity.kt
+++ app/src/main/java/student/testing/system/ui/LaunchActivity.kt
@@ -2,12 +2,14 @@ package student.testing.system.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 import student.testing.system.R
 
 @AndroidEntryPoint
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
     }
