5a74ddc08c3cd90fea2ba1cfcd39b4003e6cdc7b
Std1o <37378410+Std1o@users.noreply.github.com>
2022-05-25 00:25:28 +0300

    fragment binding delegate

--- app/src/main/java/student/testing/system/common/Extensions.kt
+++ app/src/main/java/student/testing/system/common/Extensions.kt
@@ -1,6 +1,13 @@
 package student.testing.system.common
 
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 fun View.showIf(visible: Boolean) {
     visibility = if (visible) {
@@ -9,3 +16,22 @@ fun View.showIf(visible: Boolean) {
         View.GONE
     }
 }
+
+/** Fragment binding delegate, may be used since onViewCreated up to onDestroyView (inclusive) */
+fun <T : ViewBinding> Fragment.viewBinding(factory: (View) -> T): ReadOnlyProperty<Fragment, T> =
+    object : ReadOnlyProperty<Fragment, T>, DefaultLifecycleObserver {
+        private var binding: T? = null
+
+        override fun getValue(thisRef: Fragment, property: KProperty<*>): T =
+            binding ?: factory(requireView()).also {
+                // if binding is accessed after Lifecycle is DESTROYED, create new instance, but don't cache it
+                if (viewLifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+                    viewLifecycleOwner.lifecycle.addObserver(this)
+                    binding = it
+                }
+            }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            binding = null
+        }
+    }
--- app/src/main/java/student/testing/system/ui/fragments/LoginFragment.kt
+++ app/src/main/java/student/testing/system/ui/fragments/LoginFragment.kt
@@ -1,11 +1,8 @@
 package student.testing.system.ui.fragments
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -16,24 +13,20 @@ import kotlinx.coroutines.launch
 import student.testing.system.R
 import student.testing.system.api.network.DataState
 import student.testing.system.common.AccountSession
+import student.testing.system.common.viewBinding
 import student.testing.system.databinding.FragmentLoginBinding
 import student.testing.system.ui.activity.MainActivity
 import student.testing.system.viewmodels.LoginViewModel
 
 
 @AndroidEntryPoint
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private val viewModel by viewModels<LoginViewModel>()
-    private lateinit var _binding: FragmentLoginBinding
-    private val binding get() = _binding
+    private val binding by viewBinding(FragmentLoginBinding::bind)
 
-    @SuppressLint("SetTextI18n")
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.btnSignUp.setOnClickListener {
             val bundle = Bundle()
             Navigation.findNavController(binding.root)
@@ -43,8 +36,6 @@ class LoginFragment : Fragment() {
         binding.btnLogin.setOnClickListener() {
             auth(binding.login.text.toString(), binding.password.text.toString())
         }
-
-        return binding.root
     }
 
     private fun auth(email: String, password: String) {
