6e2d64f9798c1078139c1b22ed005ad4eda267a3
Ilya <37378410+Std1o@users.noreply.github.com>
2022-09-11 10:01:13 +0300

    Update Utils.kt

--- app/src/main/java/student/testing/system/common/Utils.kt
+++ app/src/main/java/student/testing/system/common/Utils.kt
@@ -6,19 +6,18 @@ import org.json.JSONException
 import org.json.JSONObject
 import java.nio.charset.Charset
 
-class Utils {
-    companion object {
-        @Throws(JSONException::class)
-        fun encodeErrorCode(errorBody: ResponseBody?): String {
-            if (errorBody == null) {
-                return "Unknown error"
-            }
-            val source = errorBody.source()
-            val buffer: Buffer = source.buffer()
-            val charset: Charset = Charset.forName("UTF-8")
-            val errorJson = JSONObject(buffer.readString(charset))
-            val detail: String = errorJson.getString("detail")
-            return detail
+object Utils {
+
+    @Throws(JSONException::class)
+    fun encodeErrorCode(errorBody: ResponseBody?): String {
+        if (errorBody == null) {
+            return "Unknown error"
         }
+        val source = errorBody.source()
+        val buffer: Buffer = source.buffer()
+        val charset: Charset = Charset.forName("UTF-8")
+        val errorJson = JSONObject(buffer.readString(charset))
+        val detail: String = errorJson.getString("detail")
+        return detail
     }
 }
