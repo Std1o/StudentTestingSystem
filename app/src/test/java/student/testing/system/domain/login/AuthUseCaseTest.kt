e836ecb7724a1268ce0c97a3b137efa1b0785f11
Ilya <37378410+Std1o@users.noreply.github.com>
2022-09-12 12:34:31 +0300

    upd AuthUseCaseTest

--- app/src/test/java/student/testing/system/domain/login/AuthUseCaseTest.kt
+++ app/src/test/java/student/testing/system/domain/login/AuthUseCaseTest.kt
@@ -1,6 +1,5 @@
 package student.testing.system.domain.login
 
-import com.google.common.truth.Truth.assertThat
 import io.mockk.mockk
 import io.mockk.spyk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -30,16 +29,30 @@ class AuthUseCaseTest {
     }
 
     @Test
+    fun `invalid E-mail format returns EmailError`() = runTest {
+        val expected = LoginState.EmailError(R.string.error_invalid_email)
+        val actual = authUseCase.invoke("someEmail", "").first()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `empty password returns PasswordError`() = runTest {
+        val expected = LoginState.PasswordError(R.string.error_empty_field)
+        val actual = authUseCase.invoke("test@mail.ru", "").first()
+        assertEquals(expected, actual)
+    }
+
+    @Test
     fun `success auth returns PrivateUser`() = runBlocking {
         val expected = LoginState.Success(PrivateUser(1, "Ivan", "test@mail.ru", "some_token"))
         val actual = authUseCase.invoke("test@mail.ru", "pass").first()
-        assertThat(actual).isEqualTo(expected)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun `failed auth returns Error`() = runBlocking {
         val expected = LoginState.Error("Incorrect username or password")
         val actual = authUseCase.invoke("other@mail.ru", "pass").first()
-        assertThat(actual).isEqualTo(expected)
+        assertEquals(expected, actual)
     }
 }
