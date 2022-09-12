b9cc3dd0dacd6d51106f3800f6b2a82aaf0608fa
Ilya <37378410+Std1o@users.noreply.github.com>
2022-09-13 00:56:46 +0300

    Update GetResultUseCaseTest.kt

--- app/src/test/java/student/testing/system/domain/getResult/GetResultUseCaseTest.kt
+++ app/src/test/java/student/testing/system/domain/getResult/GetResultUseCaseTest.kt
@@ -8,16 +8,29 @@ import org.junit.Assert.*
 import org.junit.Test
 import student.testing.system.FakeRepository
 
+
+@ExperimentalCoroutinesApi
 class GetResultUseCaseTest {
 
     private val repository = FakeRepository()
     private val getResultUseCase = spyk(GetResultUseCase(repository))
 
-    @ExperimentalCoroutinesApi
     @Test
     fun `when 404 returns NoResult state`() = runTest {
         val expected = ResultState.NoResult
         val actual = getResultUseCase(1, 1).first()
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `all errors except 404 returns Error state`() = runTest {
+        val actual = getResultUseCase(-1, 1).first()
+        assertTrue(actual is ResultState.Error)
+    }
+
+    @Test
+    fun `request may return success`() = runTest {
+        val actual = getResultUseCase(13, 1).first()
+        assertTrue(actual is ResultState.Success)
     }
 }
