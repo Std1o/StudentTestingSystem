c7ddca13b3211bdeba3b6f355fb746a746161ed7
ilya <37378410+Std1o@users.noreply.github.com>
2023-09-24 00:48:37 +0300

    upd BottomNavigation

--- app/src/main/java/student/testing/system/presentation/ui/screens/CourseReviewScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/CourseReviewScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -47,8 +49,9 @@ fun CoursesScreen() {
                         },
                         label = {
                             Text(
-                                stringResource(screen.stringRes),
-                                color = if (selected) Purple500 else Color.Black
+                                text = stringResource(screen.stringRes),
+                                fontSize = 12.sp,
+                                color = if (selected) Purple500 else Color.Black,
                             )
                         },
                         selected = selected,
