fe5d333e2accbf8717054e14a07782ae1b8d6993
ilya <37378410+Std1o@users.noreply.github.com>
2023-09-25 12:02:49 +0300

    Update SimpleUIStateHandler.kt

--- app/src/main/java/student/testing/system/presentation/ui/components/SimpleUIStateHandler.kt
+++ app/src/main/java/student/testing/system/presentation/ui/components/SimpleUIStateHandler.kt
@@ -3,7 +3,6 @@ package student.testing.system.presentation.ui.components
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.CoroutineScope
 import student.testing.system.annotations.NotScreenState
 import student.testing.system.domain.states.DataState
 import student.testing.system.presentation.viewmodels.ResettableViewModel
@@ -16,14 +15,25 @@ fun <T> SimpleUIStateHandler(
     viewModel: ResettableViewModel,
     onSuccess: @Composable (T) -> Unit,
 ) {
-    if (uiState is DataState.Loading) {
-        LoadingIndicator()
-    } else if (uiState is DataState.Success) {
-        onSuccess.invoke(uiState.data)
-    } else if (uiState is DataState.Error) {
-        LaunchedEffect(Unit) { // the key define when the block is relaunched
-            snackbarHostState.showSnackbar(uiState.exception)
+    when (uiState) {
+        is DataState.Loading -> LoadingIndicator()
+
+        is DataState.Success -> {
+            onSuccess.invoke(uiState.data)
             viewModel.resetState()
+        }
+
+        is DataState.Error -> {
+            LaunchedEffect(Unit) { // the key define when the block is relaunched
+                snackbarHostState.showSnackbar(uiState.exception)
+                viewModel.resetState()
+            }
+        }
+
+        is DataState.Empty, // it mustn't reach here, it must be replaced with Success in the ViewModel
+        is DataState.NoState,
+        is DataState.ValidationError -> { // will be moved to individual state
+            // do nothing
         }
     }
 }
