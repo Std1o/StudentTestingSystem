7bbde6700b537e29a9c98ca4629fbd04b40c0860
ilya <37378410+Std1o@users.noreply.github.com>
2023-10-07 15:36:02 +0300

    fixed JoinCourseUseCaseTest

--- app/src/test/java/student/testing/system/domain/usecases/JoinCourseUseCaseTest.kt
+++ app/src/test/java/student/testing/system/domain/usecases/JoinCourseUseCaseTest.kt
@@ -3,6 +3,8 @@
 package student.testing.system.domain.usecases
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
@@ -22,20 +24,22 @@ class JoinCourseUseCaseTest {
 
     @Test
     fun `empty courseCode returns ValidationError`() = runTest {
-        val actual = joinCourseUseCase("")
+        val actual = joinCourseUseCase("").first()
         assertTrue(actual is ValidatableOperationState.ValidationError)
     }
 
     @Test
     fun `failed response returns Error`() = runTest {
-        val actual = joinCourseUseCase("QQQQQQ")
-        assertTrue(actual is RequestState.Error)
+        val actual = joinCourseUseCase("QQQQQQ").toList()
+        assertTrue(actual[0] is ValidatableOperationState.SuccessfulValidation)
+        assertTrue(actual[1] is RequestState.Error)
     }
 
     @Test
     fun `success response returns CourseResponse`() = runTest {
-        val actual = joinCourseUseCase("5TYHKW")
-        assertTrue(actual is RequestState.Success)
-        assertThat((actual as RequestState.Success).data, instanceOf(CourseResponse::class.java))
+        val actual = joinCourseUseCase("5TYHKW").toList()
+        assertTrue(actual[0] is ValidatableOperationState.SuccessfulValidation)
+        assertTrue(actual[1] is RequestState.Success)
+        assertThat((actual[1] as RequestState.Success).data, instanceOf(CourseResponse::class.java))
     }
 }
