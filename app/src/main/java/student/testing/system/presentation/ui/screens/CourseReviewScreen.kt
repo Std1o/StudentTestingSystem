7f5dcb0199247d3b2b569eeccb3e5545d7596d3c
ilya <37378410+Std1o@users.noreply.github.com>
2023-10-13 01:26:54 +0300

    tests decomposition

--- app/src/main/java/student/testing/system/presentation/ui/screens/CourseReviewScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/CourseReviewScreen.kt
@@ -26,6 +26,7 @@ import student.testing.system.presentation.navigation.Destination
 import student.testing.system.presentation.navigation.NavHost
 import student.testing.system.presentation.navigation.composable
 import student.testing.system.presentation.ui.activity.ui.theme.Purple500
+import student.testing.system.presentation.ui.screens.tests.TestsScreen
 import student.testing.system.presentation.viewmodels.CourseReviewViewModel
 import student.testing.system.presentation.viewmodels.CourseSharedViewModel
 
--- app/src/main/java/student/testing/system/presentation/ui/screens/tests/TestsScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/tests/TestsScreen.kt
@@ -1,22 +1,13 @@
-package student.testing.system.presentation.ui.screens
+package student.testing.system.presentation.ui.screens.tests
 
 import android.app.Activity
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -35,27 +26,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 import student.testing.system.R
-import student.testing.system.annotations.NotScreenState
 import student.testing.system.common.AccountSession
-import student.testing.system.common.iTems
 import student.testing.system.domain.states.LoadableData
-import student.testing.system.models.Test
-import student.testing.system.presentation.ui.activity.ui.theme.Purple700
 import student.testing.system.presentation.ui.components.CenteredColumn
 import student.testing.system.presentation.ui.components.ConfirmationDialog
 import student.testing.system.presentation.ui.components.LastOperationStateUIHandler
-import student.testing.system.presentation.ui.components.Shimmer
-import student.testing.system.presentation.ui.components.modifiers.placeholder
 import student.testing.system.presentation.ui.fragments.TestsFragment
 import student.testing.system.presentation.viewmodels.CourseSharedViewModel
 import student.testing.system.presentation.viewmodels.TestsViewModel
@@ -145,61 +128,4 @@ fun TestsScreen(parentViewModel: CourseSharedViewModel) {
         }
     }
     LastOperationStateUIHandler(lastOperationStateWrapper, snackbarHostState)
-}
-
-@OptIn(NotScreenState::class, ExperimentalFoundationApi::class)
-@Composable
-fun TestsList(
-    isLoading: Boolean,
-    hidden: Boolean,
-    tests: LoadableData<List<Test>>,
-    onClick: (Test) -> Unit,
-    onLongClick: (Test) -> Unit
-) {
-    if (hidden) return
-    val data = (tests as? LoadableData.Success)?.data
-    val mockTests = listOf(Test(id = 0), Test(id = 1), Test(id = 2))
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        iTems(data ?: mockTests, key = { it }) { test ->
-            val shape = RoundedCornerShape(5.dp)
-            Card(
-                elevation = 10.dp,
-                shape = shape,
-                modifier = Modifier
-                    .animateItemPlacement()
-                    .padding(vertical = 10.dp, horizontal = 16.dp)
-                    .fillMaxWidth()
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(shape)
-                        .combinedClickable(
-                            onClick = { onClick(test) },
-                            onLongClick = { onLongClick(test) },
-                        )
-                ) {
-                    Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
-                        Text(
-                            text = test.name,
-                            modifier = Modifier
-                                .widthIn(min = 60.dp)
-                                .clip(CircleShape)
-                                .placeholder(isLoading, Shimmer()),
-                            fontSize = 14.sp,
-                            color = Purple700
-                        )
-                        Text(
-                            text = test.creationTime,
-                            modifier = Modifier
-                                .padding(top = 4.dp)
-                                .clip(CircleShape)
-                                .widthIn(min = 100.dp)
-                                .placeholder(isLoading, Shimmer()),
-                            color = Color.DarkGray
-                        )
-                    }
-                }
-            }
-        }
-    }
 }
