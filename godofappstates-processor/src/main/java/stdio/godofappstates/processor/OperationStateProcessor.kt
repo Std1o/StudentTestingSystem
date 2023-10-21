e5da5595aa8eb3877fb789384e7094c898fe457a
ilya <37378410+Std1o@users.noreply.github.com>
2023-10-22 00:55:34 +0300

    upped ksp version, NO_OPERATION_STATE_ANNOTATION and NO_LOADABLE_DATA_ANNOTATION errors

--- godofappstates-processor/src/main/java/stdio/godofappstates/processor/OperationStateProcessor.kt
+++ godofappstates-processor/src/main/java/stdio/godofappstates/processor/OperationStateProcessor.kt
@@ -8,6 +8,7 @@ import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.validate
+import stdio.godofappstates.Constants
 import stdio.godofappstates.annotations.LoadableData
 import stdio.godofappstates.annotations.OperationState
 import stdio.godofappstates.visitors.LoadableDataKClassVisitor
@@ -21,6 +22,7 @@ class OperationStateProcessor(
     override fun process(resolver: Resolver): List<KSAnnotated> {
         val dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray())
 
+        // OperationState
         val symbolsOperationState = resolver
             .getSymbolsWithAnnotation(OperationState::class.qualifiedName!!)
         symbolsOperationState.filter { it is KSClassDeclaration && it.validate() }
@@ -28,12 +30,22 @@ class OperationStateProcessor(
                 it.accept(OperationStateKClassVisitor(codeGenerator, logger, dependencies), Unit)
             }
 
+        // LoadableData
         val symbolsLoadableData = resolver
             .getSymbolsWithAnnotation(LoadableData::class.qualifiedName!!)
         symbolsLoadableData.filter { it is KSClassDeclaration && it.validate() }
             .forEach {
                 it.accept(LoadableDataKClassVisitor(codeGenerator, logger, dependencies), Unit)
             }
+
+        val symbolsOperationStateList = symbolsOperationState.toList()
+        val symbolsLoadableDataList = symbolsLoadableData.toList()
+        // Processor is runs twice. First runs with annotations and second start without.
+        // That's why we can't ruin building relying on annotation symbols emptiness
+        if ((symbolsOperationStateList + symbolsLoadableDataList).isNotEmpty()) {
+            if (symbolsOperationStateList.isEmpty()) logger.error(Constants.NO_OPERATION_STATE_ANNOTATION)
+            if (symbolsLoadableDataList.isEmpty()) logger.error(Constants.NO_LOADABLE_DATA_ANNOTATION)
+        }
 
         val unableToProcessOperationsState = symbolsOperationState.filterNot { it.validate() }
         val unableToProcessLoadableData = symbolsLoadableData.filterNot { it.validate() }
