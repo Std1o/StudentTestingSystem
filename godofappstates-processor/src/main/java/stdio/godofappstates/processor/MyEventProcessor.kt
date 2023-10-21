f2dba991f7562ba9d54b985631b525b76eac2fb4
ilya <37378410+Std1o@users.noreply.github.com>
2023-10-21 15:26:11 +0300

    full OperationState generated

--- godofappstates-processor/src/main/java/stdio/godofappstates/processor/MyEventProcessor.kt
+++ godofappstates-processor/src/main/java/stdio/godofappstates/processor/MyEventProcessor.kt
@@ -63,25 +63,42 @@ class MyEventProcessor(
                 """
     |package $packageName
 
-    |import $classPackage
-    |import android.os.Bundle
-
-    |sealed interface OperationState<out R> : $interfaceName<R> {
+    |import stdio.godofappstates.annotations.FunctionalityState
+    |import student.testing.system.domain.operationTypes.OperationType
     |
-    |data class Success<out T>(
-    |    val data: T
-    ) : OperationState<T>
-    |  
-    |   /**
-    | * Must be converted to Success with own local value in ViewModel
+    |/**
+    | * OperationState contains the result of operation and is not intended for long-term storage of the screen state.
+    | *
+    | * Its value must either be passed to the ContentState,
+    | * or used for some quick actions (show the loader, show the snackbar with an error, navigate to a new screen)
     | */
-    |data class Empty(
-    |    val code: Int
-    ) : OperationState<Nothing>
+    |@FunctionalityState
+    |sealed interface OperationState<out R> : $interfaceName<R> {
+    |    data object NoState : OperationState<Nothing>
     |
-    | data class Error(val exception: String, val code: Int = -1) : OperationState<Nothing>
-    | 
-    |  object Loading : OperationState<Nothing>
+    |    data class Success<out T>(
+    |        val data: T,
+    |        val operationType: OperationType = OperationType.DefaultOperation
+    |    ) : OperationState<T>
+    |
+    |    /**
+    |     * Must be converted to Success with own local value in ViewModel.
+    |     *
+    |     * Or you have to handle it by yourself
+    |     */
+    |    data class Empty204(
+    |        val code: Int,
+    |        val operationType: OperationType = OperationType.DefaultOperation
+    |    ) : OperationState<Nothing>
+    |
+    |    data class Error(
+    |        val exception: String,
+    |        val code: Int = -1,
+    |        val operationType: OperationType = OperationType.DefaultOperation
+    |    ) : OperationState<Nothing>
+    |
+    |    data class Loading(val operationType: OperationType = OperationType.DefaultOperation) :
+    |        OperationState<Nothing>
     |}
     """.trimMargin().toByteArray()
             )
