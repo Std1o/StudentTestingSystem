d7e29520b930d96e6b5a4be279c864a493c366f8
ilya <37378410+Std1o@users.noreply.github.com>
2023-10-22 18:32:18 +0300

    added but not used exception WrongFunctionReturnType

--- app/src/main/java/student/testing/system/common/Exceptions.kt
+++ app/src/main/java/student/testing/system/common/Exceptions.kt
@@ -1,3 +1,12 @@
 package student.testing.system.common
 
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
 class NullSharedViewModelException : RuntimeException("SharedViewModel is null")
+
+class WrongFunctionReturnType(kType: KType) : RuntimeException(
+    "Expected: OperationState or OperationState superclass\n" +
+            "Found: $kType\n\n" +
+            "Please make sure that you use sealed interface marked with annotation @OperationState"
+)
