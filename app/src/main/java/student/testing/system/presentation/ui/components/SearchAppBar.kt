eac5e4c6d71cb1bb44c6f8de938c67c6173398f3
ilya <37378410+Std1o@users.noreply.github.com>
2023-11-03 23:11:47 +0300

    simple SearchAppBar

---
+++ app/src/main/java/student/testing/system/presentation/ui/components/SearchAppBar.kt
@@ -0,0 +1,75 @@
+package student.testing.system.presentation.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import student.testing.system.R
+
+@Composable
+fun SearchAppBar(onQueryChanged: (String) -> Unit) {
+    // Immediately update and keep track of query from text field changes.
+    var query: String by rememberSaveable { mutableStateOf("") }
+    var showClearIcon by rememberSaveable { mutableStateOf(false) }
+
+    if (query.isEmpty()) {
+        showClearIcon = false
+    } else if (query.isNotEmpty()) {
+        showClearIcon = true
+    }
+
+    TextField(
+        value = query,
+        onValueChange = { newQuery ->
+            // If user makes changes to text, immediately updated it.
+            query = newQuery
+            // To avoid crash, only query when string isn't empty.
+            if (newQuery.isNotEmpty()) {
+                // Pass latest query to refresh search results.
+                onQueryChanged(newQuery)
+            }
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                tint = MaterialTheme.colors.onBackground,
+                contentDescription = "Search Icon"
+            )
+        },
+        trailingIcon = {
+            if (showClearIcon) {
+                IconButton(onClick = { query = "" }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Clear,
+                        tint = MaterialTheme.colors.onBackground,
+                        contentDescription = "Clear Icon"
+                    )
+                }
+            }
+        },
+        maxLines = 1,
+        placeholder = { Text(text = stringResource(R.string.hint_search_query)) },
+        textStyle = MaterialTheme.typography.subtitle1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.background, shape = RectangleShape)
+    )
+}
--- app/src/main/java/student/testing/system/presentation/ui/screens/resultsReview/ResultsReviewScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/resultsReview/ResultsReviewScreen.kt
@@ -1,17 +1,20 @@
 package student.testing.system.presentation.ui.screens.resultsReview
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import kotlinx.coroutines.launch
 import student.testing.system.domain.states.loadableData.LoadableData
 import student.testing.system.models.Test
 import student.testing.system.presentation.ui.components.CenteredColumn
+import student.testing.system.presentation.ui.components.SearchAppBar
 import student.testing.system.presentation.viewmodels.ResultsViewModel
 
 @Composable
@@ -19,13 +22,25 @@ fun ResultsReviewScreen(test: Test) {
     val viewModel = hiltViewModel<ResultsViewModel>()
     val contentState by viewModel.contentState.collectAsState()
     Surface {
-        CenteredColumn {
-            Text(text = "Просмотр результатов")
-            ResultsList(
-                isLoading = contentState.results is LoadableData.Loading,
-                hidden = false,
-                results = contentState.results,
-            )
+        Scaffold(
+            topBar = {
+                SearchAppBar {
+                    println(it)
+                }
+            }
+        ) { contentPadding ->
+            CenteredColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding)
+            ) {
+                Text(text = "Просмотр результатов")
+                ResultsList(
+                    isLoading = contentState.results is LoadableData.Loading,
+                    hidden = false,
+                    results = contentState.results,
+                )
+            }
         }
     }
     LaunchedEffect(key1 = Unit) {
