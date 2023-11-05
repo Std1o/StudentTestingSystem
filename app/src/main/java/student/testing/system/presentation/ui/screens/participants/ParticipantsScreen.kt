31c428dc1564c3e1842b5fcad34ba571638aeda9
ilya <37378410+Std1o@users.noreply.github.com>
2023-11-05 14:14:41 +0300

    delete participant ConfirmationDialog

--- app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import student.testing.system.R
 import student.testing.system.domain.states.operationStates.OperationState
 import student.testing.system.models.Participant
 import student.testing.system.presentation.ui.components.CenteredColumn
+import student.testing.system.presentation.ui.components.ConfirmationDialog
 import student.testing.system.presentation.ui.components.LastOperationStateUIHandler
 import student.testing.system.presentation.viewmodels.CourseSharedViewModel
 import student.testing.system.presentation.viewmodels.ParticipantsViewModel
@@ -20,6 +25,7 @@ fun ParticipantsScreen(parentViewModel: CourseSharedViewModel) {
     val course by parentViewModel.courseFlow.collectAsState()
     val viewModel = hiltViewModel<ParticipantsViewModel>()
     val lastOperationState by viewModel.lastOperationState.collectAsState()
+    var deletingParticipantId by remember { mutableStateOf<Int?>(null) }
     Surface {
         CenteredColumn {
             ParticipantsList(
@@ -33,8 +39,18 @@ fun ParticipantsScreen(parentViewModel: CourseSharedViewModel) {
                         viewModel.addModerator(course.id, id)
                     }
                 },
-                onDelete = { viewModel.deleteParticipant(course.id, it) })
+                onDelete = { deletingParticipantId = it })
         }
+    }
+    deletingParticipantId?.let {
+        ConfirmationDialog(
+            onDismissRequest = { deletingParticipantId = null },
+            onConfirmation = {
+                viewModel.deleteParticipant(course.id, it)
+                deletingParticipantId = null
+            },
+            dialogText = stringResource(id = R.string.delete_request)
+        )
     }
     with(lastOperationState) {
         if (this is OperationState.Success) {
