c5f628d66452799e117912ccc5fa7fceb3f5a123
ilya <37378410+Std1o@users.noreply.github.com>
2023-11-05 13:52:27 +0300

    ParticipantsScreen decomposition

--- app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
@@ -1,41 +1,14 @@
 package student.testing.system.presentation.ui.screens.participants
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import student.testing.system.R
-import student.testing.system.common.AccountSession
-import student.testing.system.common.iTems
 import student.testing.system.domain.states.operationStates.OperationState
 import student.testing.system.models.Participant
-import student.testing.system.presentation.ui.activity.ui.theme.BlueColor
-import student.testing.system.presentation.ui.activity.ui.theme.GrayColor
-import student.testing.system.presentation.ui.components.Avatar
 import student.testing.system.presentation.ui.components.CenteredColumn
 import student.testing.system.presentation.ui.components.LastOperationStateUIHandler
 import student.testing.system.presentation.viewmodels.CourseSharedViewModel
@@ -65,6 +38,7 @@ fun ParticipantsScreen(parentViewModel: CourseSharedViewModel) {
     }
     with(lastOperationState) {
         if (this is OperationState.Success) {
+            @Suppress("UNCHECKED_CAST")
             parentViewModel.setCourse(course.copy(participants = data as List<Participant>))
         }
     }
@@ -73,80 +47,4 @@ fun ParticipantsScreen(parentViewModel: CourseSharedViewModel) {
         { viewModel.onErrorReceived() },
         snackbarHostState
     )
-}
-
-@Composable
-fun ParticipantsList(
-    hidden: Boolean,
-    participants: List<Participant>,
-    currentParticipant: Participant,
-    onAppointModerator: (Int, Boolean) -> Unit,
-    onDelete: (Int) -> Unit
-) {
-    if (hidden) return
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        iTems(participants, key = { it }) { participant ->
-            val shape = RoundedCornerShape(5.dp)
-            Card(
-                elevation = 10.dp,
-                shape = shape,
-                modifier = Modifier
-                    .padding(vertical = 10.dp, horizontal = 16.dp)
-                    .fillMaxWidth()
-            ) {
-                Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Avatar(participant.username)
-                        Column {
-                            Box(modifier = Modifier.fillMaxWidth()) {
-                                Row(
-                                    modifier = Modifier.align(Alignment.CenterStart),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Text(
-                                        text = participant.username,
-                                        fontSize = 17.sp,
-                                        color = Color.DarkGray,
-                                    )
-                                    if (participant.isOwner || participant.isModerator) {
-                                        Icon(
-                                            imageVector = ImageVector.vectorResource(R.drawable.ic_star),
-                                            tint = if (participant.isOwner) BlueColor else GrayColor,
-                                            contentDescription = "",
-                                            modifier = Modifier
-                                                .padding(start = 3.dp)
-                                                .size(12.dp)
-                                        )
-                                    }
-                                }
-                                if (currentParticipant.isOwner && !participant.isOwner) {
-                                    ParticipantContextMenu(
-                                        modifier = Modifier
-                                            .align(Alignment.CenterEnd),
-                                        onAppointModerator = {
-                                            onAppointModerator(
-                                                participant.userId,
-                                                participant.isModerator
-                                            )
-                                        },
-                                        onDelete = { onDelete(participant.userId) }
-                                    )
-                                }
-                            }
-                            Row {
-                                Spacer(modifier = Modifier.weight(1f))
-                                Text(
-                                    text = participant.email,
-                                    modifier = Modifier
-                                        .widthIn(min = 100.dp),
-                                    fontSize = 14.sp,
-                                    color = Color.DarkGray
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
