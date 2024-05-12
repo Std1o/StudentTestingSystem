75c099812fea042fb51f0ce13c21e10962221e02
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-12 03:59:39 +0300

    chucker

--- app/src/main/java/student/testing/system/di/NetworkModule.kt
+++ app/src/main/java/student/testing/system/di/NetworkModule.kt
@@ -1,17 +1,21 @@
 package student.testing.system.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import student.testing.system.common.Constants
-import student.testing.system.data.api.CourseManagementApi
 import student.testing.system.data.OAuthInterceptor
 import student.testing.system.data.api.AuthApi
+import student.testing.system.data.api.CourseManagementApi
 import student.testing.system.data.api.CoursesApi
 import student.testing.system.data.api.TestsApi
 import java.util.concurrent.TimeUnit
@@ -26,21 +30,27 @@ object NetworkModule {
         HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
     @Provides
-    fun getHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder =
+    fun getHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        @ApplicationContext context: Context
+    ): OkHttpClient.Builder =
         OkHttpClient.Builder()
             .callTimeout(1, TimeUnit.MINUTES)
             .retryOnConnectionFailure(true)
             .addInterceptor(OAuthInterceptor())
             .addInterceptor(httpLoggingInterceptor)
+            .addNetworkInterceptor(ChuckerInterceptor(context))
 
     @Provides
     @Singleton
-    fun provideRetrofit(httpBuilder: OkHttpClient.Builder): Retrofit =
-        Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
+    fun provideRetrofit(httpBuilder: OkHttpClient.Builder): Retrofit {
+        val builder = GsonBuilder().disableHtmlEscaping().create()
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(builder))
             .baseUrl(Constants.BASE_URL)
             .client(httpBuilder.build())
             .build()
+    }
 
     @Provides
     @Singleton
