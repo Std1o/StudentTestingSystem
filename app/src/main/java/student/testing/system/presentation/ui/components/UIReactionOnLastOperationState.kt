c16d1168a239c243fe10755e7f8d884b90d9f71f
Std1o <37378410+Std1o@users.noreply.github.com>
2024-12-09 01:02:36 +0300

    fix ParticipantsScreen snackbar

--- app/src/main/java/student/testing/system/presentation/ui/components/UIReactionOnLastOperationState.kt
+++ app/src/main/java/student/testing/system/presentation/ui/components/UIReactionOnLastOperationState.kt
@@ -29,7 +29,7 @@ fun <T> UIReactionOnLastOperationState(
     with(event) {
         when (this) {
             is OperationState.ErrorSingle -> {
-                LaunchedEffect(Unit) { // the key define when the block is relaunched
+                LaunchedEffect(operationState) { // the key define when the block is relaunched
                     onError?.invoke(exception, code, operationType)
                         ?: snackbarHostState.showSnackbar(exception)
                 }
--- app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
+++ app/src/main/java/student/testing/system/presentation/ui/screens/participants/ParticipantsScreen.kt
@@ -1,6 +1,9 @@
 package student.testing.system.presentation.ui.screens.participants
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -8,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import student.testing.system.R
@@ -27,8 +31,12 @@ fun ParticipantsScreen(parentViewModel: CourseSharedViewModel) {
     val events by viewModel.events.collectAsState(OperationState.NoState)
     val lastOperationState by viewModel.lastOperationState.collectAsState()
     var deletingParticipantId by remember { mutableStateOf<Int?>(null) }
-    Surface {
-        CenteredColumn {
+    Scaffold (
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+    ) { contentPadding ->
+        CenteredColumn(modifier = Modifier.padding(contentPadding)) {
             ParticipantsList(
                 hidden = false,
                 participants = course.participants,
