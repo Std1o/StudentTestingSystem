44ee22de2c892666f0099c1a2c6df9042ad7a683
Std1o <37378410+Std1o@users.noreply.github.com>
2025-01-11 14:31:27 +0300

    start of websockets

---
+++ app/src/main/java/student/testing/system/data/api/KtorWebsocketClient.kt
@@ -0,0 +1,125 @@
+package student.testing.system.data.api
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.utils.EmptyContent
+import io.ktor.client.utils.EmptyContent.headers
+import io.ktor.http.HttpHeaders
+import io.ktor.http.headers
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import student.testing.system.common.AccountSession
+import java.util.concurrent.TimeUnit
+
+class KtorWebsocketClient(
+    private val url: String,
+    private val listener: WebsocketEvents,
+) {
+    private val client = HttpClient(OkHttp) {
+        defaultRequest {
+            header(HttpHeaders.Authorization, "Bearer ${AccountSession.instance.token!!}")
+        }
+
+        engine {
+            config {
+                pingInterval(PING_INTERVAL, TimeUnit.MILLISECONDS)
+            }
+        }
+
+        install(Logging)
+        install(WebSockets)
+    }
+
+    private val scope = CoroutineScope(Dispatchers.IO) + SupervisorJob() + CoroutineExceptionHandler { _, throwable ->
+            Log.d(TAG, "Error: ${throwable.message}")
+        }
+
+    private var job: Job? = null
+
+    private var session: WebSocketSession? = null
+
+    suspend fun connect() {
+        try {
+            Log.d(TAG, "Connecting to websocket at $url...")
+
+            session = client.webSocketSession(url)
+
+            listener.onConnected()
+
+            Log.i(TAG, "Connected to websocket at $url")
+
+            session!!.incoming
+                .receiveAsFlow()
+                .filterIsInstance<Frame.Text>()
+                .filterNotNull()
+                .collect { data ->
+                    val message = data.readText()
+                    listener.onReceive(message)
+
+                    Log.i(TAG, "Received message: $message")
+                }
+        } catch (e: Exception) {
+            listener.onDisconnected(e.message ?: "Unknown error")
+
+            Log.d(TAG, "Error: ${e.message}")
+
+            reconnect()
+        }
+    }
+
+    private fun reconnect() {
+        job?.cancel()
+        Log.d(TAG, "Reconnecting to websocket in ${RECONNECT_DELAY}ms...")
+
+        job = scope.launch {
+            stop()
+            delay(RECONNECT_DELAY)
+            connect()
+        }
+    }
+
+    suspend fun stop() {
+        Log.d(TAG, "Closing websocket session...")
+        session?.close()
+        session = null
+    }
+
+    suspend fun send(message: String) {
+        Log.d(TAG, "Sending message: $message")
+
+        session?.send(Frame.Text(message))
+    }
+
+    interface WebsocketEvents{
+        fun onReceive(data: String)
+        fun onConnected()
+        fun onDisconnected(reason: String)
+    }
+
+    companion object {
+        private const val RECONNECT_DELAY = 10_000L
+        private val PING_INTERVAL = 5_000L
+
+        private const val TAG = "WebSocketClient"
+    }
+}
--- app/src/main/java/student/testing/system/presentation/viewmodels/ResultsViewModel.kt
+++ app/src/main/java/student/testing/system/presentation/viewmodels/ResultsViewModel.kt
@@ -2,21 +2,32 @@ package student.testing.system.presentation.viewmodels
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import lilith.presentation.viewmodel.StatesViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import lilith.presentation.viewmodel.StatesViewModel
 import stdio.lilith.core.delegates.StateFlowVar.Companion.stateFlowVar
-import student.testing.system.domain.repository.TestsRepository
-import student.testing.system.domain.states.loadableData.LoadableData
+import student.testing.system.common.Constants.BASE_URL
+import student.testing.system.data.api.KtorWebsocketClient
+import student.testing.system.data.api.KtorWebsocketClient.WebsocketEvents
 import student.testing.system.domain.models.Test
 import student.testing.system.domain.models.TestResultsRequestParams
+import student.testing.system.domain.repository.TestsRepository
+import student.testing.system.domain.states.loadableData.LoadableData
 import student.testing.system.presentation.ui.models.FiltersContainer
 import student.testing.system.presentation.ui.models.contentState.ResultsContentState
 import javax.inject.Inject
 
+@Suppress("UNREACHABLE_CODE")
 @HiltViewModel
-class ResultsViewModel @Inject constructor(private val repository: TestsRepository) :
+class ResultsViewModel @Inject constructor(
+    private val repository: TestsRepository
+) :
     StatesViewModel() {
 
     private val _contentState = MutableStateFlow(ResultsContentState())
@@ -43,7 +54,11 @@ class ResultsViewModel @Inject constructor(private val repository: TestsReposito
             )
         }
 
-        viewModelScope.launch {
+        val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
+            throwable.printStackTrace()
+        }
+
+        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
             loadData { repository.getResults(test.id, test.courseId, params) }.collect {
                 contentStateVar = contentStateVar.copy(results = it)
                 if (it is LoadableData.Success && filtersContainer.maxScore == 0) {
@@ -53,6 +68,25 @@ class ResultsViewModel @Inject constructor(private val repository: TestsReposito
                     }
                 }
             }
+        }
+
+        // TODO relocate
+        viewModelScope.launch {
+            val ggg = KtorWebsocketClient(url = "wss://testingsystem.ru/tests/ws/results/58?course_id=1",
+                    object : WebsocketEvents {
+                        override fun onReceive(data: String) {
+                            println("onReceive")
+                        }
+
+                        override fun onConnected() {
+                            println("onConnected")
+                        }
+
+                        override fun onDisconnected(reason: String) {
+                            println("onDisconnected")
+                        }
+                    })
+            ggg.connect()
         }
     }
 }
