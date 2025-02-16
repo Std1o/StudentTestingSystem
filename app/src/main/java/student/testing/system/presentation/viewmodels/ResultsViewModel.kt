9ee6d3bd00f3142e55a985741a8a723321444cf8
Std1o <37378410+Std1o@users.noreply.github.com>
2025-02-16 10:10:39 +0300

    Update ResultsViewModel.kt

--- app/src/main/java/student/testing/system/presentation/viewmodels/ResultsViewModel.kt
+++ app/src/main/java/student/testing/system/presentation/viewmodels/ResultsViewModel.kt
@@ -1,21 +1,17 @@
 package student.testing.system.presentation.viewmodels
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import lilith.presentation.viewmodel.StatesViewModel
 import stdio.lilith.core.delegates.StateFlowVar.Companion.stateFlowVar
 import student.testing.system.data.api.KtorWebsocketClientImpl
 import student.testing.system.domain.models.ParticipantsResults
 import student.testing.system.domain.models.Test
 import student.testing.system.domain.models.TestResultsRequestParams
-import student.testing.system.domain.repository.TestsRepository
 import student.testing.system.domain.states.loadableData.LoadableData
 import student.testing.system.domain.webSockets.KtorWebsocketClient
 import student.testing.system.domain.webSockets.WebsocketEvents
@@ -25,12 +21,8 @@ import javax.inject.Inject
 import kotlin.properties.Delegates
 
 
-@Suppress("UNREACHABLE_CODE")
 @HiltViewModel
-class ResultsViewModel @Inject constructor(
-    private val repository: TestsRepository,
-) :
-    StatesViewModel() {
+class ResultsViewModel @Inject constructor() : ViewModel() {
 
     private val _contentState = MutableStateFlow(ResultsContentState())
     val contentState = _contentState.asStateFlow()
@@ -41,16 +33,7 @@ class ResultsViewModel @Inject constructor(
     val filtersContainer = FiltersContainer()
     var isConnected: Boolean by Delegates.observable(false) { _, _, new ->
         if (new) {
-            val params = with(filtersContainer) {
-                TestResultsRequestParams(
-                    onlyMaxResult = showOnlyMaxResults, searchPrefix = searchPrefix,
-                    upperBound = if (ratingRangeEnabled) upperBound else null,
-                    lowerBound = if (ratingRangeEnabled) lowerBound else null,
-                    scoreEquals = if (scoreEqualsEnabled) scoreEqualsValue else null,
-                    dateFrom = dateFrom, dateTo = dateTo, ordering = orderingType
-                )
-            }
-
+            val params = getTestResultsRequestParams()
             viewModelScope.launch {
                 val jsonObject = Gson().toJson(params)
                 client.send(jsonObject)
@@ -69,12 +52,7 @@ class ResultsViewModel @Inject constructor(
                     contentStateVar = contentStateVar.copy(
                         results = LoadableData.Success(dataJson)
                     )
-                    if (filtersContainer.maxScore == 0) {
-                        filtersContainer.maxScore = dataJson.maxScore
-                        if (filtersContainer.maxScore == 0) {
-                            filtersContainer.maxScore = 100 // this can happen if there are no results
-                        }
-                    }
+                    configureMaxScore(dataJson)
                 }
 
                 override fun onConnected() {
@@ -97,6 +75,25 @@ class ResultsViewModel @Inject constructor(
 
         viewModelScope.launch {
             client.connect()
+        }
+    }
+
+    private fun getTestResultsRequestParams() = with(filtersContainer) {
+        TestResultsRequestParams(
+            onlyMaxResult = showOnlyMaxResults, searchPrefix = searchPrefix,
+            upperBound = if (ratingRangeEnabled) upperBound else null,
+            lowerBound = if (ratingRangeEnabled) lowerBound else null,
+            scoreEquals = if (scoreEqualsEnabled) scoreEqualsValue else null,
+            dateFrom = dateFrom, dateTo = dateTo, ordering = orderingType
+        )
+    }
+
+    private fun configureMaxScore(participantsResults: ParticipantsResults) {
+        if (filtersContainer.maxScore == 0) {
+            filtersContainer.maxScore = participantsResults.maxScore
+            if (filtersContainer.maxScore == 0) {
+                filtersContainer.maxScore = 100 // this can happen if there are no results
+            }
         }
     }
 
