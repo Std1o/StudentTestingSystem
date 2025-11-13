0857a823a53d6e5e7ef8e6085605906bc5a570d3
Std1o <37378410+Std1o@users.noreply.github.com>
2025-11-13 21:13:51 +0300

    upd apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "student.testing.system"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
