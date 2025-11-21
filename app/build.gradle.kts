002738d8a9c994512e2b81c921e1b8a4fd871a81
Std1o <37378410+Std1o@users.noreply.github.com>
2025-11-21 16:25:11 +0300

    upd apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "student.testing.system"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
